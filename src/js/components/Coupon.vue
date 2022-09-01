f63b992adb235fa10e1f41eeb05b4db792a1749b
Nitipat L <nitipat.l@linecorp.com>
2022-09-01 20:32:34 +0700

    [Added] init project

---
+++ src/js/components/Coupon.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container text-center">
+      <img src="https://vos.line-scdn.net/strapi-cluster-instance-bucket-83/image_e72b33b56d.svg" class="img-responsive">
+      <div v-if="!isLoading">
+        <input type="couponcode" class="form-control text-center mb-2" placeholder="Coupon Code" v-model="coupon">
+        <button type="submit" class="btn btn-success w-100" @click="redeem">Redeem</button>
+      </div>
+      <div v-else>
+        <div class="spinner-grow text-success">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'redaxios'
+export default {
+  data() {
+    return {
+      coupon: '',
+      isLoading: false,
+    }
+  },
+  methods: {
+    redeem() {
+      if (this.coupon == '') {
+        this.$swal({
+          icon: 'warning',
+          text: 'Please input coupon!',
+        })
+        return
+      }
+      this.isLoading = true
+      axios.post(import.meta.env.VITE_API_ENDPOINT_URL, {
+        coupon: this.coupon
+      }).then(result => {
+        this.isLoading = false
+        if (result.data.result === 'pass') {
+          this.$swal({
+            icon: 'success',
+            title: 'Successful!',
+            text: `Quota: ${result.data.quota}, Available: ${result.data.available}`,
+          })
+        } else {
+          this.$swal({
+            icon: 'error',
+            text: 'Coupon is invalid!',
+          })
+        }
+      }).catch(() => {
+        this.isLoading = false
+        this.$swal({
+          icon: 'error',
+          text: 'Something went wrong!',
+        })
+      })
+      this.coupon = ''
+    }
+  }
+}
+</script>
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue'
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    hot: true
+  }
+}
