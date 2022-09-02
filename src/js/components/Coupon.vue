500603d1d84c97b803b4647cb61936f1535a0a39
Nitipat L <nitipat.l@linecorp.com>
2022-09-02 19:17:00 +0700

    [Edited] update text & image

--- src/js/components/Coupon.vue
+++ src/js/components/Coupon.vue
@@ -1,14 +1,59 @@
 <template>
-  <div class="container text-center">
-      <img src="https://vos.line-scdn.net/strapi-cluster-instance-bucket-83/image_e72b33b56d.svg" class="img-fluid">
-      <div v-if="!isLoading">
-        <input type="couponcode" class="form-control text-center mb-2" placeholder="Coupon Code" v-model="coupon">
+  <div class="container text-center mobile">
+      <img src="/images/line-dev-logo.png" class="img-fluid">
+      <img src="/images/logo.png" class="img-fluid">
+      
+      <div class="card p-2">
+        <div class="card-body">
+          <p>
+          <b><span style="color:green">ยินดีต้อนรับสู่บูธ</span> <span style="color:blue">LINE SHOPPING API</span></b>
+          <br />
+          ทีมงานได้เตรียมตัวอย่างการประยุกต์ใช้
+          ในการสร้างระบบขาย digital code
+          </p>
+          <p>
+            โดยใช้หน้าร้านของ LINE SHOPPING เป็นช่องทางในการให้ลูกค้าของร้านมาสั่งซื้อ ชำระเงิน และได้รับ code จากหน้าแช้ทกับ Official Account ของร้าน และสามารถนํา code ไปใช้ที่ website ของร้านได้
+          </p>
+          <p>
+            <b>ลองใช้ของจริงกัน ลองเสร็จ แลกคะแนนกิจกรรมประจำบูธไปเลย!</b>
+          </p>
+        </div>
+      </div>
+      <div class="card mt-2 p-2">
+        <img src="images/step-01.png" class="card-img-top">
+        <div class="card-body">
+          <p class="card-text">
+            เข้าไปซื้อ Digital code ที่ร้านของ LINE SHOPPING API บน LINE SHOPPING<br />
+<a href="https://shop.line.me/@655kwkxg/product/1002648355" target="_blank">https://shop.line.me/@655kwkxg/product/1002648355</a><br /><br />
+
+ไม่ต้องจ่ายเงินจริง<br />
+แนบสลิปโอนเงินด้วยรูปอะไรก็ได้<br />
+เมื่อทีมงานยืนยันคำสั่งซื้อ คุณจะได้โค้ดผ่านห้องแช้ทกับ LINE Official Account
+          </p>
+        </div>
+      </div>
+      <div class="card mt-2 p-2">
+        <img src="images/step-02.png" class="card-img-top">
+        <div class="card-body">
+          <p class="card-text">
+            copy code ของคุณ แล้วนำมา paste ที่นี่<br />
+แล้วกด Redeem เพื่อแลกคะแนนกิจกรรม<br />
+ไปได้เลย!<br />
+          </p>
+        </div>
+      </div>
+      <div v-if="!isLoading" class="mt-2">
+        <input type="couponcode" class="form-control text-center mb-2" placeholder="paste code ที่นี่" v-model="coupon">
         <button type="submit" class="btn btn-success w-100" @click="redeem">Redeem</button>
       </div>
-      <div v-else>
+      <div v-else class="mt-2">
         <div class="spinner-grow text-success">
           <span class="visually-hidden">Loading...</span>
         </div>
+      </div>
+      <div class="card mt-2 p-2">
+        <a href="https://feedback.line.me/enquete/public/16977-STbvntks" target="blank">ลงทะเบียนเพื่อเปิดใช้งาน API กับร้านของคุณได้ที่นี่</a> หรือ <a href="https://oaplus.line.biz/information/api-doc">ศึกษา API Document ได้ที่นี่ครับ</a>
+<br /><b>ขอบคุณที่มาเยี่ยมชมครับ</b>
       </div>
   </div>
 </template>
@@ -40,7 +85,10 @@ export default {
           this.$swal({
             icon: 'success',
             title: 'Successful!',
-            text: `Quota: ${result.data.quota}, Available: ${result.data.available}`,
+            html: `
+            Quota: ${result.data.quota}, Available: ${result.data.available}<br />
+            <span style="color:green;font-weight:bold;">ยื่นจอนี้ให้ทีมงานที่บูธเพื่อรับคะแนนได้เลย</span>
+            `,
           })
         } else {
           this.$swal({
@@ -60,3 +108,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.mobile {
+  max-width:720px;
+}
+</style>
